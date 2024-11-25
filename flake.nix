a725adfcd63aabac4c639eb211024f8268a6ded6
Pwoodlock <patrick@devsec.ie>
2024-11-25 00:35:44 +0000

    Refactor

--- flake.nix
+++ flake.nix
@@ -1,27 +1,4 @@
-# First in a line of Flake configuration files etc for Github CI/CD
-#
-# https://librephoenix.com/2023-10-21-intro-flake-config-setup-for-new-nixos-users.html
-# 
-# nix flake update
-# sudo nixos-rebuild switch --flake .#NAMEOFCONFIGURATION
-#
-# ****** HOME-MANAGER *********
-# 
-#
-#
-#
-# *** The below is the Nix Channel for Unstable for Home Manager
-# nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-# nix-channel --update
-# nix-shell '<home-manager>' -A install
-#
-#  To rebuild and update you type
-#
-# home-manager switch --flake .  (Which is for the default user)
-# And then theres a another for that type of configuration
-# 
-#  home-manager switch --flake .#nixos-dd which is what my user here is called at the moment.
-#
+# Daily Driver NixOS Machine Alpha 
 
 
 {
